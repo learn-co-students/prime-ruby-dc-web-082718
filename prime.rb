8ed64346057d4beb6f445ea5eb2b182016d08f63
Trevor Jameson <tjameson15@gmail.com>
2018-07-08 19:15:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+def prime?(int)
+  if !(int > 1)
+    return false
+  elsif int > 1
+    counter = int
+    while counter > 1
+      if int % counter == 0
+        return true
+      end
+      counter += 1
+    end
+  end
+end
