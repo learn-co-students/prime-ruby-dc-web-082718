4579e0cd3adc2b12bd866b35933d7d687724f8d4
Trevor Jameson <tjameson15@gmail.com>
2018-07-29 15:08:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,15 +1,19 @@
-# Add  code here!
+require 'pry'
 
 def prime?(int)
   if !(int > 1)
     return false
   elsif int > 1
-    counter = int
+    counter = int - 1
     while counter > 1
+      # binding.pry
       if int % counter == 0
-        return true
+        return false
       end
-      counter += 1
+      counter -= 1
     end
   end
+  return true
 end
+
+prime?(4)
